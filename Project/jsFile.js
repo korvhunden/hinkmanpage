8c2423f09930126ce712e7ee06029d10482a9104
korvhunden <josef_skywakman@hotmail.com>
2017-05-06 13:52:09 +0200

    Added files from laptop

---
+++ Project/jsFile.js
@@ -0,0 +1,6 @@
+
+  function hideEverything() {
+    document.getElementById("interactive_element").style.display = "none";
+    global_highscore.style.display = "none";
+    personal_highscore.style.display = "none";
+  }
