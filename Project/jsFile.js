72bfe039325a455816e3bfac73a2852295d14c38
korvhunden <josef_skywakman@hotmail.com>
2017-05-17 22:59:22 +0200

    Added updated files, with jquery

--- Project/jsFile.js
+++ Project/jsFile.js
@@ -1,6 +1,11 @@
 
-  function hideEverything() {
-    document.getElementById("interactive_element").style.display = "none";
-    global_highscore.style.display = "none";
-    personal_highscore.style.display = "none";
-  }
+  // function hideEverything() {
+  //   document.getElementById("interactive_element").style.display = "none";
+  //   global_highscore.style.display = "none";
+  //   personal_highscore.style.display = "none";
+  // }
+  //
+
+$(document).ready(function) {
+  
+}
